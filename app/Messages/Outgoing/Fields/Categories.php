1c10e589a46c7762d6014632015e1c1513016d1b
Lenin Paulino <leninpaulinon@gmail.com>
2020-07-13 01:37:49 -0400

    feat: add Categories question type

---
+++ app/Messages/Outgoing/Fields/Categories.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Messages\Outgoing\Fields;
+
+use App\Messages\Outgoing\SelectQuestion;
+use BotMan\BotMan\Messages\Incoming\Answer;
+use Illuminate\Validation\Rule;
+
+class Categories extends SelectQuestion
+{
+    public function __construct(array $field)
+    {
+        parent::__construct($field, 'id', 'tag');
+    }
+
+    public function getRules(): array
+    {
+        $validationRules = [];
+
+        if ($this->field['required']) {
+            $validationRules[] = 'required';
+        }
+
+        $validationRules[] = 'array';
+
+        $rules = [
+        $this->field['key']  => $validationRules,
+        $this->field['key'].'.*'  => Rule::in(array_keys($this->optionsMap)),
+      ];
+
+        return $rules;
+    }
+
+    public function getAnswerBody(Answer $answer): array
+    {
+        $value = $answer->isInteractiveMessageReply() ? $answer->getValue() : $answer->getText();
+
+        return [$this->field['key'] => explode(',', $value)];
+    }
+
+    public function getAnswerResponse(): array
+    {
+        $values = [];
+        foreach ($this->answerValue as $option) {
+            $values[] = $this->optionsMap[$option];
+        }
+
+        return [
+        'value' => $values,
+      ];
+    }
+}
