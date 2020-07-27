1fb5dae89bb354a1ff579d2f927ddf5cfd947acf
Lenin Paulino <leninpaulinon@gmail.com>
2020-07-26 23:52:06 -0400

    test: add some test helpers for validation errors

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,6 +5,8 @@ namespace Tests;
 use App\BotManTester;
 use BotMan\BotMan\BotMan;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Validation\ValidationException;
+use Throwable;
 
 abstract class TestCase extends BaseTestCase
 {
@@ -19,4 +21,25 @@ abstract class TestCase extends BaseTestCase
      * @var BotManTester
      */
     protected $bot;
+
+    protected function getFirstErrorMessage(ValidationException $exception)
+    {
+        $errors = $exception->validator->errors()->all();
+
+        return count($errors) ? $errors[0] : null;
+    }
+
+    /**
+     * Assert the exception is a validation exception and contains provided string
+     * as part of the error.
+     *
+     * @param string $error
+     * @param ValidationException $exception
+     * @return void
+     */
+    protected function assertValidationError(string $error, Throwable $exception)
+    {
+        $this->assertInstanceOf(ValidationException::class, $exception);
+        $this->assertStringContainsString($error, $this->getFirstErrorMessage($exception));
+    }
 }
